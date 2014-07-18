b9c9468c51e33d6f12ffc2392f6bd29ae95069a2
fenglong <fenglong@xiaomi.com>
2014-07-18 15:33:21 +0800

    ftp 上传

--- server.js
+++ server.js
@@ -2,14 +2,57 @@ var http = require('http');
 var formidable = require('formidable');
 var fs = require('fs');
 var path = require('path');
+var _exists = fs.existsSync || path.existsSync;
 var mkdirp = require('mkdirp');
 var PORT = parseInt(process.argv[2]) || 8999;
+
+var JSFtp = require("jsftp"), ftp_config = {};
+JSFtp = require('jsftp-mkdirp')(JSFtp);
+
+/*
+.ftppass 文件
+
+{
+  host: "myserver.com",
+  port: 3331, // defaults to 21
+  user: "user", // defaults to "anonymous"
+  pass: "1234" // defaults to "@anonymous"
+}
+
+*/
+
+var ftpPassPath = "./.ftppass";
+
+if(_exists(ftpPassPath)){
+    var ftp_content = fs.readFileSync(ftpPassPath);
+    ftp_config = (new Function('return ' + ftp_content))();
+    console.log('ftp load ' + JSON.stringify(ftp_config));
+/*
+    var to = 'test/test.js';
+    fs.readFile('./server.js', function (err, data) {
+        var buffer = new Buffer(data, "binary");
+        ftp_client.mkdirp(path.dirname(to), function (err) {
+            console.log(err);
+            ftp_client.put(buffer, to, function(err) {
+                if (err) {
+                    console.log(err);
+                } else {
+                }
+            });
+
+        });
+    });
+*/
+}
+
+
 
 var server = http.createServer(function (req, res) {
     
     function error(err) {
         res.writeHead(500, {'Content-Type': 'text/plain'});
         res.end(err.toString()); //fail
+        console.log('error: ' + err.toString());
     }
 
     function next(from, to) {
@@ -27,6 +70,8 @@ var server = http.createServer(function (req, res) {
             }
         });
     }
+
+    console.log(req.url);
 
     if (req.url == '/') {
         // show a file upload form
@@ -62,9 +107,58 @@ var server = http.createServer(function (req, res) {
                 });
             }
         });
+    } else if (req.url == '/receiver/ftp' && req.method.toLowerCase() == 'post') {
+        var form = new formidable.IncomingForm();
+        form.parse(req, function (err, fields, files) {
+            if (err) {
+                error(err);
+            } else {
+                var to = fields['to'];
+
+                fs.readFile(files.file.path, function (err, data) {
+                    if (err) {
+                        console.log('error file: ' + to);
+                        //error(err);
+                        res.writeHead(200, {'Content-Type': 'text/plain'});
+                        res.end('0'); //success
+                    } else {
+                        var buffer = new Buffer(data, "binary");
+                        try{
+                            var ftp_client = new JSFtp(ftp_config);
+                            ftp_client.mkdirp(path.dirname(to), function (err) {
+                                if (err) {
+                                    error(err);
+                                }
+                                console.log(to);
+                                ftp_client.put(buffer, to, function(err) {
+                                    if (err) {
+                                        error(err);
+                                    } else {
+                                        res.writeHead(200, {'Content-Type': 'text/plain'});
+                                        res.end('0'); //success
+                                    }
+
+                                    ftp_client.raw.quit();
+                                });
+
+                            });
+                        }catch(ex){
+                            console.log('ex');
+                        }
+                    }
+                });
+            }
+        });
     }
+
 });
 
 server.listen(PORT, function () {
     console.log('receiver listening *:' + PORT);
+});
+
+
+process.on('uncaughtException', function (err) {
+    console.log("Node NOT Exiting...");
+    console.error(err.stack);
 });
